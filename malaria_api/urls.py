2ac70296d49ee7e8199ec24b8c4bc03d947a5a81
Tuntufye4 <tuntumwanyongo@gmail.com>
2025-11-10 09:07:07 +0200

    rem debug_toolbar

--- malaria_api/urls.py
+++ malaria_api/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin   
 from django.conf import settings   
 from django.urls import path, include
 
@@ -16,8 +16,8 @@ urlpatterns = [
 ]     
         
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]   
+#if settings.DEBUG:
+ #   import debug_toolbar
+ #   urlpatterns += [
+ #       path('__debug__/', include(debug_toolbar.urls)),
+ #   ]   
